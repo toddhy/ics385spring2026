c34a66c8b1abc5b50b999ebfb930d1cb32f34a4b
Todd Yoshioka <toddhy@hawaii.edu>
2026-02-13 10:41:08 -1000

    implement VolCalc which is a webpage that calculates volume of cylinder given height and radius.

---
+++ week5/calculator/VolCalc/VolCalc.js
@@ -0,0 +1,43 @@
+/*
+Name: Todd Yoshioka
+Class: ICS 395
+Notes: Uses Express to first read index.html using GET, then takes user input from page and calculates volume of 
+a cylinder given height and radius. Answer is sent to page at /VolCalc/ using POST. It's rounded to 2 decimal places
+and displayed as text for user to read. 
+*/
+
+//load modules
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//create app object
+const app = express();
+
+// Not sure what bodyparser does, need to read docs. When I remove it I get errors.
+app.use(bodyParser.urlencoded({extended: true}));
+
+//this sends the html file to the web page using the root directory
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+});
+
+// handles the VolCalc form submission
+// loads subpage url /VolCalc/ when button is pushed
+app.post("/VolCalc/", function (req, res) {
+
+  //converts the string input to a float number
+  //Captures string from the height and radius values from body of index.html
+  var rad = parseFloat(req.body.radius);
+  var hgt = parseFloat(req.body.height);
+
+  // computes volume of cylinder given height and radius 
+  var vol = Math.PI * (rad ** 2) * hgt;
+
+  //displays the result to /VolCalc/
+  res.send("Volume of cylinder is " + vol.toFixed(2));
+});
+
+//start server
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
--- week5/calculator/f2c/f2cCalc.js
+++ week5/calculator/f2c/f2cCalc.js
@@ -1,3 +1,10 @@
+/*
+Name: Todd Yoshioka
+Class: ICS 395
+Notes: Uses Express to first read index.html using GET, then takes user input from page and calculates Farenheit
+to Celsius. Answer is converted to integer and sent to page at /f2c/ using POST. It's displayed there for user to read. 
+*/
+
 //load modules
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -5,7 +12,7 @@ const bodyParser = require("body-parser");
 //create app object
 const app = express();
 
-// not sure what this does?
+// Not sure what bodyparser does, need to read docs. When I remove it I get errors.
 app.use(bodyParser.urlencoded({extended: true}));
 
 //this sends the html file to the web page using the root directory
@@ -16,17 +23,20 @@ app.get("/", function(req, res) {
 // handles the F2C form submission
 // loads subpage url /f2c/ when button is pushed
 app.post("/f2c/", function (req, res) {
+
   //converts the string input to a float number
   //Captures string from the "Far" name in body of index.html
   var far = parseFloat(req.body.Far);
 
-  // computes celsius from given farenheit
+  // computes celsius from given farenheit and converts to integer
   var cels = (far - 32) * 5 / 9;
+  var cels = Math.round(cels);
 
-  //display the result in 2 decimal places
-  res.send("Celsius is " + cels.toFixed(2));
+  //display the result to /f2c/
+  res.send("Conversion to Celsius is " + cels);
 });
 
+//start express server
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
